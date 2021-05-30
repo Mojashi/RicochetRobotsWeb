b1b49de44dde9a0126117638b462c6688819930a
Mojashi <akihiro1001@gmail.com>
2021-05-30 13:40:59 +0900

    support deb

--- setup.sh
+++ setup.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+
+if [ $# -ne 1 ]; then
+  echo "you need to specify build-type" 1>&2
+  exit 1
+fi
+if [ $1 = dev ]; then
+  cp .env_showcase_dev .env
+else
+  cp .env_showcase .env
+fi
+echo -e DB=$MARIADB_DATABASE >> .env
+echo -e DB_USER=$MARIADB_USERNAME >> .env
+echo -e DB_PASS=$MARIADB_PASSWORD >> .env
+echo -e DB_HOST=$MARIADB_HOSTNAME >> .env
+
 rm -rf /usr/local/go
 rm -rf /usr/sbin/go
 # if [ ! -e go1.16.4.linux-amd64.tar.gz ];then
